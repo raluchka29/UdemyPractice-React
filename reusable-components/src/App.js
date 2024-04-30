7e73a7eb636f09a04990dbc504dbbd79773bda3d
Raluca <raluca.elena.butacu@gmail.com>
2024-04-30 15:59:57 +0300

    reusable first-contact

--- reusable-components/src/App.js
+++ reusable-components/src/App.js
@@ -1,8 +1,20 @@
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./img/alexa.png";
+import CortanaImage from "./img/cortana.png";
+import SiriImage from "./img/siri.png";
 
+console.log(AlexaImage);
+console.log(SiriImage);
 
 function App() {
   return (
-   
+    <div>
+      <div>Personal Digital Assistants</div>
+     
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+    </div>
   );
 }
 
