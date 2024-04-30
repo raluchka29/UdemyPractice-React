20463b41b3fa4f86f3c1483c4141788e341b3596
Raluca <raluca.elena.butacu@gmail.com>
2024-04-30 16:41:14 +0300

    personal digital assistance mini-project done

--- reusable-components/src/App.js
+++ reusable-components/src/App.js
@@ -1,3 +1,4 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
 import AlexaImage from "./img/alexa.png";
 import CortanaImage from "./img/cortana.png";
@@ -9,11 +10,41 @@ console.log(SiriImage);
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
+      <section className='hero is-primary'>
+        <div className='hero-body'>
+        <p className='title'>Personal Digital Assistants</p>
+        </div>
+      </section>
+      
      
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
-      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage} />
-      <ProfileCard title="Siri" handle="@siri01" image={SiriImage} />
+     <div className='container'>
+      <section className='section'>
+        <div className='columns'>
+          <div className='column is-3'>
+          <ProfileCard 
+          title="Alexa" 
+          handle="@alexa99" 
+          image={AlexaImage} 
+          description="Alexa was created by Amazon and helps you buy things."/>
+          </div>
+          <div className='column is-3'>
+          <ProfileCard 
+          title="Cortana" 
+          handle="@cortana32" 
+          image={CortanaImage}
+          description="Cortana was made by Microsoft. Who knows what is does?" />
+            </div>
+            <div className='column is-3'>
+            <ProfileCard 
+            title="Siri" 
+            handle="@siri01" 
+            image={SiriImage}
+            description="Siri was made by Apple and is the most private intelligent assistant." />
+            </div>
+        </div>
+      </section>
+     </div>        
+
     </div>
   );
 }
