628316a25878af323ba49f936a2a5a4ec6126fb8
Raluca <raluca.elena.butacu@gmail.com>
2024-05-04 00:57:24 +0300

    usestate miniApp

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,16 +1,33 @@
 import {useState} from 'react';
+import AnimalShow from './AnimalShow';
+import './App.css';
+
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)]
+}
+
 
 function App() {
-  const[count, setCount] = useState(0);
-
-const handleClick = () => {
-  setCount(count+1);
+  const [animals, setAnimals] = useState([]);
+  
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()])
 };
 
+const renderedAnimals = animals.map((animal, index) => {
+  return <AnimalShow type={animal} key={index}/>
+} )
+
   return (
-    <div>
+    <div className='app'>
       <button onClick={handleClick}>Add Animal</button>
-      <div>Number of animals: {count}</div>
+      <div className='animal-list'>
+        {renderedAnimals}
+      </div>
+      
     </div>
   );
 }
