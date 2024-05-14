bf636050586d3913b34be83eea85e925f04ad623
Raluca <raluca.elena.butacu@gmail.com>
2024-05-15 00:45:03 +0300

    updating states + styles

--- books/src/App.js
+++ books/src/App.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
 
 
 function App() {
+   const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+
+    };
+
   return (
-    <div>App</div>
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
   )}
   
 
--- books/src/components/BookCreate.js
+++ books/src/components/BookCreate.js
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import {useState} from 'react';
 
-function BookCreate() {
-    const [books, setBooks] = useState([]);
+function BookCreate({onCreate}) {
+    const [title, setTitle] = useState('');
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+        setTitle('');
+    }
 
-    return <div>BookCreate</div>
+    return <div className='book-create'>
+        <h3>Add a Book!</h3>
+        <form onSubmit={handleSubmit}>
+            <label>Title</label>
+            <input className='input' value={title} onChange={handleChange} />
+            <button className='button'>Create!</button>
+        </form>
+    </div>
 }
 
 export default BookCreate;
