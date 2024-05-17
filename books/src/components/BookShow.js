d51144906a368b2bb71df91a6119397b6b6d0eba
Raluca <raluca.elena.butacu@gmail.com>
2024-05-17 14:47:30 +0300

    edit + delete button

--- books/src/components/BookShow.js
+++ books/src/components/BookShow.js
@@ -1,5 +1,35 @@
-function BookShow() {
-    return <div>BookShow</div>
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+
+function BookShow({ book, onDelete, onEdit }) {
+const [showEdit, setShowEdit] = useState(false);
+
+    const handleDeleteClick = () => {
+        onDelete(book.id);
+    };
+
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    };
+
+    let content = <h3>{book.title}</h3>
+    if(showEdit) {
+        content = <BookEdit onEdit={onEdit} book={book} />
+    };
+
+    return (
+        <div className="book-show">
+            <div>{content}</div>
+            <div className="actions">
+                <button className="delete" onClick={handleDeleteClick}>
+                    Delete
+                </button>
+                <button className="edit" onClick={handleEditClick}>
+                    Edit
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default BookShow;
