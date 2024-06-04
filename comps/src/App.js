5cff512bd4d01339854336f8acee77658467d224
Raluca <raluca.elena.butacu@gmail.com>
2024-06-04 23:28:09 +0300

    prop-types + intalling tailwind

---
+++ comps/src/App.js
@@ -0,0 +1,23 @@
+import Button from "./button";
+
+function App() {
+    return <div>
+        <div>
+            <Button success rounded outline>Click me!</Button>
+        </div>
+        <div>
+            <Button danger outline>Buy now!</Button>
+        </div>
+        <div>
+            <Button warning>See Deal!</Button>
+        </div>
+        <div>
+            <Button secondary outline>Add to wishlist!</Button>
+        </div>
+        <div>
+            <Button secondary rounded>Hide ads!</Button>
+        </div>
+        </div>
+}
+
+export default App;
