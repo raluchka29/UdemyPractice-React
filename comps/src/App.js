41c6edf0e19045a953cd26e3a8648539c908ad60
Raluca <raluca.elena.butacu@gmail.com>
2024-06-18 23:31:03 +0300

    useRef, dropdown complete

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,13 +1,25 @@
 import Dropdown from "./components/Dropdown";
+import { useState } from "react";
 
 function App() {
+const [selection, setSelection] =  useState(null);
+
+const handleSelect = (option) => {
+    setSelection(option);
+};
+
 const options = [
     {label: 'Red', value: 'red'},
     {label: 'Blue', value: 'blue'},
     {label: 'Green', value: 'green'}
 ];
 
-    return <Dropdown options={options} />;
+    return (
+    <div className="flex">
+    <Dropdown options={options} value={selection} onChange={handleSelect} />
+    <Dropdown options={options} value={selection} onChange={handleSelect} />  
+    </div>
+    )
 }
 
 export default App;
