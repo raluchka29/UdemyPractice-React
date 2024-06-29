b65d9a0bfad0b6ee0520406c0e52308cf620e7f5
Raluca <raluca.elena.butacu@gmail.com>
2024-06-29 23:17:58 +0300

    reusable table

--- comps/src/components/Sidebar.js
+++ comps/src/components/Sidebar.js
@@ -6,6 +6,7 @@ const links = [
     { label: 'Accordion', path: '/accordion'},
     { label: 'Buttons', path: '/buttons'},
     { label: 'Modal', path: '/modal'},
+    { label: 'Table', path: '/table'},
 ];
 const renderedLinks = links.map((link) => {
     return <Link 
---
+++ comps/src/pages/TablePage.js
@@ -0,0 +1,34 @@
+import Table from "../components/Table";
+
+function TablePage() {
+const data = [
+    { name: 'Orange' , color: 'bg-orange-500', score: 5},
+    { name: 'Banana', color: 'bg-yellow-500', score: 2},
+    { name: 'Apple', color: 'bg-red-500', score: 8},
+    { name: 'Lime', color: 'bg-green-500', score: 7},
+];
+
+const config = [
+    { label: 'Name',
+      render: (fruit) => fruit.name,
+    },
+    { label: 'Color',
+        render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>
+    },
+    { label: 'Score',
+        render: (fruit) => fruit.score,
+    },
+];
+
+const keyFn = (fruit) => {
+    return fruit.name;
+};
+
+    return (
+        <div>
+            <Table config={config} data={data} keyFn={keyFn}/>
+        </div>
+    );
+}
+
+export default TablePage;
