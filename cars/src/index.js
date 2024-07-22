e29a3defed33e49f9cf30b767415cd84de2564aa
Raluca <raluca.elena.butacu@gmail.com>
2024-07-22 14:36:29 +0300

    starting cars app

---
+++ cars/src/index.js
@@ -0,0 +1,8 @@
+import react from "react";
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const el = document.getElementById('root');
+const root = createRoot(el);
+
+root.render(<App />);
